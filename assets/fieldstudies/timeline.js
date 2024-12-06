7907e2d5d6fa25a2087a28d210514762ca83d8d3
10Rats <70593617+10Rats@users.noreply.github.com>
2024-12-05 22:44:08 -0500

    big ass update

---
+++ assets/fieldstudies/timeline.js
@@ -0,0 +1,304 @@
+var local_timeline = [
+    {
+        year: "1964",
+        description: "<cite>The Garden of the Multiverse</cite> was published for the first time.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1967",
+        description: "Unmanned Surveryor 3 was launched from Cape Kennedy to the lunar surface and encountered LBA-01 (Large Bright Anomaly).",
+        image: "./local1967.jpg"
+    },
+    {
+        year: "1969",
+        description: "Apollo 12 was launched to retrieve LBA-01, which was then housed in a facility on Merritt Island.<br><br>Project SOMA was created by DoD to study the properties of LBA-01 with the help of Arthur Reese, Amelia Haydock, Mathilda Remlinger, Dorothy Ross, Carlos Scharf, Todd Tilzer, and Alan Trenchard.",
+        image: "./local1969.jpg"
+    },
+    {
+        year: "1970",
+        description: "During New Year's, Reese, Trenchard, and Ross went missing. Afterwards, objects from Deviation Omega started to appear in local Spacetime. Field Studies Institute was created to gather and study these objects.<br><br><a href='https://fieldstudiesinstitute.org/databank/obj_archive/twin_apollo' target='_blank'>twin_apollo</a> was discovered in Orange Lake, FL.",
+        image: "./local1970.jpg"
+    },
+    {
+        year: "1971",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/payload_maneuvers' target='_blank'>payload_maneuvers</a> was discovered in Jackson, NJ.",
+        image: "./local1971.jpg"
+    },
+    {
+        year: "1972",
+        description: "Deviation remnants were discovered in FSI's second floor copy machine.<br><br><a href='https://fieldstudiesinstitute.org/databank/obj_archive/brush_of_azure' target='_blank'>brush_of_azure</a> was discovered in Eupora, MS.",
+        image: "./local1972.jpg"
+    },
+    {
+        year: "1973",
+        description: "<cite>Moonshot Hotel</cite> ceased operations.<br><br><a href='https://fieldstudiesinstitute.org/databank/obj_archive/magnetic_entanglement' target='_blank'>magnetic_entanglement</a> was discovered in Tarpum Bay, BHS.",
+        image: "./local1973.jpg"
+    },
+    { 
+        year: "1974",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/transcendental_beans' target='_blank'>transcendental_beans</a> was discovered in Valdosta, GA.<br><br><a href='https://fieldstudiesinstitute.org/databank/obj_archive/static_eclipse' target='_blank'>static_eclipse</a> was discovered in Nome, AK.",
+        image: "./local1974.jpg"
+    },
+    {
+        year: "1975",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/orbital_greenery' target='_blank'>orbital_greenery</a> was discovered in Tizimín, MEX.<br><br><a href='https://fieldstudiesinstitute.org/databank/obj_archive/looping_arrowhead' target='_blank'>looping_arrowhead</a> was discovered in Kalamazoo, MI.",
+        image: "./local1975.jpg"
+    },
+    {
+        year: "1976",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/carbon_hatchery' target='_blank'>carbon_hatchery</a> was discovered in Napo, ECU.",
+        image: "./local1976.jpg"
+    },
+    {
+        year: "1977",
+        description: "Pennington retrieved a wet paper stack in Waverly, IA.<br><br><a href='https://fieldstudiesinstitute.org/databank/obj_archive/abstract_cartography' target='_blank'>abstract_cartography</a> was discovered in Gold Point, NV.",
+        image: "./local1977.jpg"
+    },
+    {
+        year: "1978",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/stellar_oculism' target='_blank'>stellar_oculism</a> was discovered in Green Pond, FL.",
+        image: "./local1978.jpg"
+    },
+    {
+        year: "1979",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/outrigger_zero' target='_blank'>outrigger_zero</a> was discovered in Biloxi, MS.<br><br><a href='https://fieldstudiesinstitute.org/databank/obj_archive/spacefaring_tiger_prints' target='_blank'>spacefaring_tiger_prints</a> was discovered in Two Buttes, CO.",
+        image: "./local1979.jpg"
+    },
+    {
+        year: "1980",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/volcanic_sunset' target='_blank'>volcanic_sunset</a> was retrieved by Pennington in Metaline Falls, WA.",
+        image: "./local1980.jpg"
+    },
+    {
+        year: "1981",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/analog_gravitation' target='_blank'>analog_gravitation</a> was discovered in Waldheim, LA.",
+        image: "./local1981.jpg"
+    },
+    {
+        year: "1983",
+        description: "Pennington attempted to infiltrate Novak's house while disguised as a pest exterminator.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1991",
+        description: "A briefcase belonging to Randy Ward was discovered in a 1977 Dodge Tradesman in Ovando, MT.<br><br><a href='https://fieldstudiesinstitute.org/databank/obj_archive/cosmic_hostility' target='_blank'>cosmic_hostility</a> was discovered in Randy Ward's briefcase.",
+        image: "./local1991.jpg"
+    },
+    {
+        year: "1994",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/brain_civics' target='_blank'>brain_civics</a> was discovered in Seattle, WA.",
+        image: "./local1994.jpg"
+    },
+    {
+        year: "1998",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/frozen_radium_eggs' target='_blank'>frozen_radium_eggs</a> was discovered in an unknown location.",
+        image: "./local1998.jpg"
+    },
+    {
+        year: "1999",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/extraction_02' target='_blank'>extraction_02</a>, <a href='https://fieldstudiesinstitute.org/databank/obj_archive/extraction_01' target='_blank'>extraction_01</a>, <a href='https://fieldstudiesinstitute.org/databank/obj_archive/extraction_03_01' target='_blank'>extraction_03_01</a>, <a href='https://fieldstudiesinstitute.org/databank/obj_archive/extraction_03_02' target='_blank'>extraction_03_02</a>, <a href='https://fieldstudiesinstitute.org/databank/obj_archive/extraction_04_01' target='_blank'>extraction_04_01</a>, <a href='https://fieldstudiesinstitute.org/databank/obj_archive/extraction_04_02' target='_blank'>extraction_04_02</a>, were discovered.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "2002",
+        description: "<a href='https://fieldstudiesinstitute.org/databank/obj_archive/broken_trials' target='_blank'>broken_trials</a> was discovered in Boneville, GA.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "2024",
+        description: "Ashford and Novak have been apprehended and reassigned to [???]",
+        image: "./local2024.jpg"
+    }
+];
+
+var omega_timeline = [
+    {
+        year: "1963",
+        description: "JFK was assassinated.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1964",
+        description: "<cite>The Garden of the Multiverse</cite> was published for the first time.",
+        image: "./local1971.jpg"
+    },
+    {
+        year: "1969",
+        description: "Neil Armstrong arrived at the moon.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1970",
+        description: "During New Year's, Omega Arthur Reese was found dead.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1971",
+        description: "Omega Alan Trenchard and Dorothy Ross founded Trenchard-Ross.",
+        image: "./omega1971.jpg"
+    },
+    {
+        year: "1972",
+        description: "Cascade Railway Co. was founded and headquartered in Cascade, NH.<br><br><cite>Humanity on Mars Princeton Summer Conference</cite> was held, with Trenchard being invited to attend.<br><br>Trenchard-Ross mainframe experienced a significant overflow due to CLAUDE's arrival. Trenchard and Ross announced CLAUDE as part of the proposal to build the first city on Mars.",
+        image: "./local1979.jpg"
+    },
+    {
+        year: "1973",
+        description: "CLAUDE 2.0 @home was announced.",
+        image: "./local1994.jpg"
+    },
+    {
+        year: "1974",
+        description: "The Parnu Peach Accords was signed by the U.S. and Soviet Union to signify unity between the two, rendering the COOP network useless.<br><br>The abandoned networks were used by Trenchard-Ross to study the properties of The Garden.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1977",
+        description: "William Lindmore was elected as president, <cite>The Garden of the Multiverse</cite> was reprinted, and a restaurant in Ovando, MT was purchased by Trenchard-Ross.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1978",
+        description: "Miroslav Morozav and Geoff Raines are the first people to land on Mars.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1983",
+        description: "The mission to colonize Mars was created with the collaboration of CLAUDE.",
+        image: "../missing2.svg"
+
+    },
+    {
+        year: "~1984",
+        description: "Supersonic Territory was created in Florida to be used for space-related missions. Any civilians living in that area prior were forced to leave. There was an increase of unexplainable phenomenons in the area over the course of time.",
+        image: "./omega1984.jpg"
+    },
+    {
+        year: "1985",
+        description: "Lindmore's presidency term ends.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1986",
+        description: "Apollo 24 was operated by IOSET (International Organization of Space Exploration and Technology). Supplies for Mars colonization were produced and transported via the Cascade Railway system.<br><br>The Bailly Station was created by the U.S. and Soviet Union and its planetarium can be visited by tourists via the Kobayashi-Rose Orbital Platform.<br><br>Local Spacetime Arthur Reese ends up in Omega after the NYE incident.",
+        image: "./omega1986.jpg"
+    },
+    {
+        year: "1987",
+        description: "Dr. Boswell investigates the sudden dimming of stars in Ovando. She predicts that all the stars will dim at the end of 1999.",
+        image: "./local1978.jpg"
+    },
+    {
+        year: "1988",
+        description: "Colonial mission to Mars was set to be departed with 732 people on board including Trenchard and Ross, but all of them perished.",
+        image: "../missing2.svg"
+    },
+    {
+        year: "1990",
+        description: "Public's opinion of space-related projects significantly declined over time.<br><br>Dressing up as a supernatural being during Halloween was looked down upon.<br><br>Harold M. Alonso resigns from his position as director of the Cape Elizabeth Space Center due to budgetary constraints.<br><br> CDOT (Center for Decommissioning of Off-Earth Technology) was created to close down any space-related facilities.",
+        image: "./local1977.jpg"
+    },
+    {
+        year: "1999",
+        description: "Electromagnetic anomalies detected in three areas: Crescent Beach Power Station, Brunswick Hydroelectric Station, and Cape Elizabeth Space Center.<br><br>Local Reese attempts to locate Halley.",
+        image: "../missing2.svg"
+    }
+];
+
+function toggleid() {
+    const checkBox = document.getElementById("toggleeverything");
+    const background = document.getElementsByTagName("body")[0];
+    const imgs = document.getElementsByTagName("img");
+    const lines = document.getElementsByClassName("line");
+    const links = document.getElementsByTagName("a");
+    if (checkBox.checked == true){
+        document.getElementsByClassName("timeline")[0].id = "omega";
+        loadtimeline();
+        background.style.backgroundColor = "black";
+        background.style.color = "white";
+        for (let i = 0; i < lines.length; i++) {
+            lines[i].style.backgroundColor = "white";
+        }
+        for (let i = 0; i < imgs.length; i++) {
+            imgs[i].style.borderColor = "white";
+        }
+        for (let i = 0; i < links.length; i++) {
+            links[i].style.color = "red";
+        }
+    } 
+    else {
+        document.getElementsByClassName("timeline")[0].id = "local";
+        loadtimeline();
+        background.style.backgroundColor = "white";
+        background.style.color = "black";
+        for (let i = 0; i < lines.length; i++) {
+            lines[i].style.backgroundColor = "black";
+        }
+        for (let i = 0; i < imgs.length; i++) {
+            imgs[i].style.borderColor = "black";
+        }
+        for (let i = 0; i < links.length; i++) {
+            links[i].style.color = "rgb(0, 72, 255)";
+        }
+    }
+}
+
+function addhtml(i, data_year, data_description, data_image) {
+    if (i % 2 !== 0) { //true if odd
+        document.getElementById("topcontainer").innerHTML +=
+            `<div class="up" id="tl${i}">
+                <div class="line"></div>
+                <div class="body">
+                    <h1>${data_year}</h1>
+                    <div class="splittwo">
+                        <center>${data_description}</center>
+                    </div>
+                </div>
+            </div>`
+    }
+    else { //true if even
+        document.getElementById("bottomcontainer").innerHTML +=
+            `<div class="down" id="tl${i}">
+                <div class="line"></div>
+                <div class="body">
+                    <h1>${data_year}</h1>
+                    <div class="splittwo">
+                        <center>${data_description}</center>
+                    </div>
+                </div>
+            </div>`
+    }
+}
+//<img src="${data_image}">
+
+function loadtimeline() {
+    const tlid = document.getElementsByClassName("timeline")[0].id;
+    document.getElementsByClassName("timeline")[0].innerHTML = 
+        `<div class="container" style="margin-left: 400px;" id="topcontainer">
+        </div>
+        <hr>
+        <div class="container" id="bottomcontainer">
+        </div>`;
+    if (tlid == "local"){
+        for (let i = 0; i < local_timeline.length; i++) {
+            const data_year = local_timeline[i].year;
+            const data_description = local_timeline[i].description;
+            const data_image = local_timeline[i].image;
+            addhtml(i, data_year, data_description, data_image);
+        };
+    }
+    else if (tlid == "omega") { 
+        for (let i = 0; i < omega_timeline.length; i++) {
+            const data_year = omega_timeline[i].year;
+            const data_description = omega_timeline[i].description;
+            const data_image = omega_timeline[i].image;
+            addhtml(i, data_year, data_description, data_image);
+        };
+    };
+}
+
+function startup() {
+    document.getElementById('toggleeverything').checked = false;
+    toggleid();
+    window.scrollTo(0,0); //i tried
+}
